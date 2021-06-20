3d9c43e43b1f3c3be073dc64d7773277e07fe382
Md. Nazmul Hossain <30841411+MdNazmul9@users.noreply.github.com>
2021-06-20 11:23:41 +0600

    google currency converter

---
+++ currency-converter.py
@@ -0,0 +1,11 @@
+
+# Install google-currency package
+
+# pip install google-currency
+
+from google_currency import convert  
+convert('usd', 'bdt', 1) 
+
+# Output:
+
+# {"from": "USD", "to": "BDT", "amount": "85.30", "converted": true}
